c2201502990927c657f722cf4235aa9685ce6bb5
khandelwal-harsh <sayhitoharshkhandelwal@gmail.com>
2021-04-29 21:24:44 +0530

    test

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import json
 
 class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
-        if isinstance(obj, np.ndarray):
+        if isinstance(obj, numpy.ndarray):
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)
 
