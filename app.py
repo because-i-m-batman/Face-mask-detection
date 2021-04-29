5c5bcaf991bc479e74bff49ec039b113e1667a05
khandelwal-harsh <sayhitoharshkhandelwal@gmail.com>
2021-04-29 21:19:27 +0530

    test

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask,jsonify,request,send_file,jsonify
+from flask import Flask,jsonify,request,send_file
 from face_mask_detector import Face_Mask_Detection
 import cv2
 import numpy
+import json
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
 
 app = Flask(__name__)
 detector_object = Face_Mask_Detection()
@@ -16,7 +24,7 @@ def run_detection():
 	output_image = detector_object.run_face_mask_detection(input_image)
 	print('[INFO] Detection Successfully Done.')
 	# return send_file(output_image,mimetype='image/gif')
-	return jsonify({"output_image":[output_image]})
+	return json.dumps({"output_image":output_image},cls=NumpyEncoder)
 
 if __name__ == '__main__':
     app.run(host = "0.0.0.0",port = 5001)
