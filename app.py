8720c4ccd5dd3e6d20893108997ad5dfbe090f02
khandelwal-harsh <sayhitoharshkhandelwal@gmail.com>
2021-04-29 21:11:47 +0530

    test

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify,request,send_file
+from flask import Flask,jsonify,request,send_file,jsonify
 from face_mask_detector import Face_Mask_Detection
 import cv2
 import numpy
@@ -6,16 +6,17 @@ import numpy
 app = Flask(__name__)
 detector_object = Face_Mask_Detection()
 
-@app.route('/send_input', methods=['POST'])
+@app.route('/send_input', methods=['GET','POST'])
 def run_detection():
 
 	file = request.files.get("image","")
 	print('[INFO] Running Face Mask Detection.')
 	input_image = cv2.imdecode(numpy.fromstring(file.read(), numpy.uint8), cv2.IMREAD_UNCHANGED)
-	print(input_image)
+	# print(input_image)
 	output_image = detector_object.run_face_mask_detection(input_image)
 	print('[INFO] Detection Successfully Done.')
-	return send_file(output_image,mimetype='image/gif')
+	# return send_file(output_image,mimetype='image/gif')
+	return jsonify({"output_image":[output_image]})
 
 if __name__ == '__main__':
     app.run(host = "0.0.0.0",port = 5001)
